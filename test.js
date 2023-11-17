c3744fc14db6c20dd22d9ed52d2b1b8a4f953dad
Yuri Potawsky <potawsky.yuri@gmail.com>
2023-11-16 21:15:42 -0500

    InitialCommit

---
+++ test.js
@@ -0,0 +1,10 @@
+
+(func)=>{
+    var newElement = document.createElement('h2');
+    newElement.textContent = 'This element is the new element.';
+    
+    var oldElement = document.getElementById('oldElement');
+    oldElement.innerHTML="Hello";
+}
+
+(func);
